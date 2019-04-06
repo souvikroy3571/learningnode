4f18572fd6c768a25a90c7a5d52c7715c265e27a
Souvik Roy <souvikroy3571@gmail.com>
2019-04-06 23:31:09 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app = express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log =`${now}  ${req.method}  ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{});
+    next();
+});
+
+// app.use((req,res,next)=>{
+//   res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname+'/public'));
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+  // return 'test';
+});
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pussyname:'Pooja',
+    // year:new Date().getFullYear()
+  });
+});
+
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pussyname:'Pooja',
+    // year:new Date().getFullYear()
+  });
+});
+
+app.get('/bad',(req,res)=>{
+  res.send({name:'error',
+    type:['404']
+  });
+});
+
+app.listen(3000);
