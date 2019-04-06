c7857108a853c54d7e8681ef691f8a065577ca2c
Souvik Roy <souvikroy3571@gmail.com>
2019-04-07 00:45:02 +0530

    second change for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname+'/views/partials');
 app.set('view engine','hbs');
@@ -42,4 +43,4 @@ app.get('/bad',(req,res)=>{
   });
 });
 
-app.listen(3000);
+app.listen(port);
